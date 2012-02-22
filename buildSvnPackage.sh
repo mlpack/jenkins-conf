fe3d560c1b2e1f9b709759fb5247d5cfcefa00ce
Sterling Lewis Peet <sterling.peet@gatech.edu>
2012-02-22 22:17:35 +0000

    remove old cruft from building up when packages are updated

--- buildSvnPackage.sh
+++ buildSvnPackage.sh
@@ -3,5 +3,6 @@
 # Build the package specified in the argument
 
 mkdir -p tarballs
+rm build-area/*.*
 cd $1/
 svn-buildpackage --svn-builder "debuild -us -uc" --svn-non-interactive --svn-download-orig --svn-dont-purge --svn-rm-prev-dir
