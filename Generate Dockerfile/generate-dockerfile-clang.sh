a1701c1853534b9696db54b8a94d12490079619e
Ryan Curtin <ryan@ratml.org>
2018-01-07 19:59:13 -0800

    Refactor container build scripts so that container size is much smaller.

--- Generate Dockerfile/generate-dockerfile-clang.sh
+++ Generate Dockerfile/generate-dockerfile-clang.sh
@@ -3,6 +3,7 @@
 arma_version=$1
 boost_version=$2
 llvm_version=$3
+
 cat > Dockerfile <<EOF
 
 # Using debian:stretch image as base-image.
@@ -19,7 +20,7 @@ RUN apt-get update  && apt-get install -y aptitude && apt-get purge -y \
 # Installing dependencies required to run mlpack.
 RUN apt-get update  && apt-get install -y --no-install-recommends wget \
     cmake binutils-dev make txt2man git build-essential python \
-    doxygen  unzip  && \
+    doxygen unzip liblapack-dev libblas-dev libarpack2 libsuperlu-dev && \
     apt-get clean && rm -rf /usr/share/man/?? && rm -rf /usr/share/man/??_* \
     && rm -rf /var/lib/apt/lists/* && rm -rf /usr/share/locale/* && \
     rm -rf /var/cache/debconf/*-old && rm -rf /usr/share/doc/*
@@ -28,38 +29,46 @@ RUN apt-get update  && apt-get install -y --no-install-recommends wget \
 # Installing clang from source.
 WORKDIR /
 RUN wget http://masterblaster.mlpack.org:5005/$llvm_version.tar.xz && \
-    tar xvf  $llvm_version.tar.xz && rm -rf $llvm_version.tar.xz && \
-    mkdir build
-WORKDIR /build
-RUN cmake -DCMAKE_INSTALL_PREFIX=/usr  /$llvm_version && make -j8 \
-    && make install && apt-get purge -y gcc && rm -rf /$llvm_version && \
-    rm -rf /build
+    tar xvf $llvm_version.tar.xz && \
+    rm -f $llvm_version.tar.xz && \
+    cd $llvm_version && \
+    mkdir build && \
+    cd build && \
+    cmake -DCMAKE_INSTALL_PREFIX=/usr ../ && \
+    make && \
+    make install && \
+    apt-get purge -y gcc && \
+    cd .. && \
+    rm -rf $llvm_version
 
 # Installing armadillo via source-code.
 WORKDIR /
 RUN wget --no-check-certificate \
-    "http://masterblaster.mlpack.org:5005/$arma_version.tar.gz" \
-    && tar xvzf $arma_version.tar.gz && rm -rf $arma_version.tar.gz
-WORKDIR $arma_version
-RUN apt-get update && apt-get install -y liblapack-dev libblas-dev libarpack2 \
-    libsuperlu-dev && cmake -DINSTALL_LIB_DIR=/usr/lib  . && make -j8 \
-    && make install && rm -rf /$arma_version
+      "http://masterblaster.mlpack.org:5005/$arma_version.tar.gz" && \
+    tar xvzf $arma_version.tar.gz && \
+    rm -f $arma_version.tar.gz && \
+    cd $arma_version && \
+    cmake -DINSTALL_LIB_DIR=/usr/lib/ . && \
+    make && \
+    make install && \
+    cd .. && \
+    rm -rf $arma_version
+
 
 # Installing boost from source.
 WORKDIR /
-RUN wget  \
-    http://masterblaster.mlpack.org:5005/$boost_version.tar.gz \
-    && tar xvzf $boost_version.tar.gz && rm -rf $boost_version.tar.gz
-WORKDIR $boost_version
-ENV CC /usr/bin/clang
-RUN ./bootstrap.sh --with-toolset=clang --prefix=/usr/  \
-    --with-libraries=math,program_options,serialization,test && \
-    ./bjam install -j8 && rm -rf /$boost_version
-
+RUN wget \
+      http://masterblaster.mlpack.org:5005/$boost_version.tar.gz && \
+    tar xvzf $boost_version.tar.gz && \
+    rm -f $boost_version.tar.gz && \
+    cd $boost_version && \
+    ./bootstrap.sh --with-toolset=clang --prefix=/usr/ \
+        --with-libraries=math,program_options,serialization,test && \
+    ./bjam install && \
+    rm -f $boost_version/
 EOF
 
-cat >> Dockerfile <<'EOF'
-
+cat >> Dockerfile << 'EOF'
 # Creating a non-root user.
 RUN adduser --system --disabled-password --disabled-login \
    --shell /bin/sh mlpack
@@ -67,27 +76,7 @@ RUN adduser --system --disabled-password --disabled-login \
 # Hardening the containers by unsetting all SUID tags.
 RUN for i in `find / -perm 6000 -type f`; do chmod a-s $i; done
 
-
 # Changing work directory and user to mlpack.
 WORKDIR /home/mlpack
 USER mlpack
-
-# The following commands can be run to build and test different versions of
-# The following commands can be run to build and test different versions of
-# mlpack.
-
-# Downloading the source files and extracting them.
-#RUN wget -c http://www.mlpack.org/files/mlpack-2.2.0.tar.gz
-#RUN tar -xvzf mlpack-2.2.0.tar.gz
-
-
-# Making build directory.
-#RUN cd mlpack-2.2.0 && mkdir build
-#WORKDIR mlpack-2.2.0/build
-
-# Making the project.
-#RUN cmake  ../
-#RUN make
-#RUN bin/mlpack_test
-
 EOF
--- Generate Dockerfile/generate-dockerfile-gcc.sh
+++ Generate Dockerfile/generate-dockerfile-gcc.sh
@@ -4,7 +4,7 @@ arma_version=$1
 boost_version=$2
 gcc_version=$3
 
-cat > Dockerfile <<EOF
+cat > Dockerfile << EOF
 
 # Using debian's latest image as base-image.
 FROM debian:stretch
@@ -20,43 +20,56 @@ RUN apt-get update  && apt-get install -y aptitude && apt-get purge -y \
 # Installing dependencies required to run mlpack.
 RUN apt-get update  && apt-get install -y --no-install-recommends wget \
     cmake binutils-dev make txt2man git build-essential  \
-    doxygen  unzip liblapack-dev libblas-dev libarpack2 libsuperlu-dev && \
+    doxygen unzip liblapack-dev libblas-dev libarpack2 libsuperlu-dev && \
     apt-get clean && rm -rf /usr/share/man/?? && rm -rf /usr/share/man/??_* && \
     rm -rf /var/lib/apt/lists/* && rm -rf /usr/share/locale/* && \
     rm -rf /var/cache/debconf/*-old && rm -rf /usr/share/doc/*
 
 
 #Installing gcc from source
-RUN wget --no-check-certificate\
-   https://ftp.gnu.org/gnu/gcc/$gcc_version/$gcc_version.tar.gz \
-    && tar xvzf $gcc_version.tar.gz && rm -r $gcc_version.tar.gz
-WORKDIR $gcc_version
-RUN ./contrib/download_prerequisites && cd .. && mkdir objdir && cd objdir && \
-   \$PWD/../$gcc_version/configure --prefix=/usr --enable-languages=c,c++,fortran \
-    --disable-multilib --disable-bootstrap && make -j8 && make install
+RUN wget --no-check-certificate \
+   https://ftp.gnu.org/gnu/gcc/$gcc_version/$gcc_version.tar.gz && \
+   tar xvzf $gcc_version.tar.gz && \
+   rm -f $gcc_version.tar.gz && \
+   cd $gcc_version && \
+   ./contrib/download_prerequisites && \
+   mkdir objdir && \
+   cd objdir && \
+   ../configure --prefix=/usr --enable-languages=c,c++,fortran \
+     --disable-multilib --disable-bootstrap && \
+   make && \
+   make install && \
+   cd ../ && \
+   rm -rf $gcc_version
 
 # Installing armadillo via source-code.
-WORKDIR /
 RUN wget --no-check-certificate \
-    http://masterblaster.mlpack.org:5005/$arma_version.tar.gz \
-    && tar xvzf $arma_version.tar.gz && rm -r $arma_version.tar.gz
-WORKDIR $arma_version
-RUN cmake -DINSTALL_LIB_DIR=/usr/lib . && make -j8  && make install
+    http://masterblaster.mlpack.org:5005/$arma_version.tar.gz && \
+    tar xvzf $arma_version.tar.gz && \
+    rm -f $arma_version.tar.gz && \
+    cd $arma_version && \
+    cmake -DINSTALL_LIB_DIR=/usr/lib . && \
+    make && \
+    make install && \
+    cd .. && \
+    rm -rf $arma_version
 
 # Installing boost from source
-WORKDIR /
 RUN wget --no-check-certificate \
-    "http://masterblaster.mlpack.org:5005/$boost_version.tar.gz" \
-    && tar xvzf $boost_version.tar.gz && rm -r $boost_version.tar.gz
+      "http://masterblaster.mlpack.org:5005/$boost_version.tar.gz" && \
+    tar xvzf $boost_version.tar.gz && \
+    rm -f $boost_version.tar.gz && \
+    cd $boost_version && \
+    ./bootstrap.sh --prefix=/usr/ \
+        -with-libraries=math,program_options,serialization,test && \
+    ./bjam install && \
+    cd ../ && \
+    rm -rf $boost_version
 WORKDIR $boost_version
-RUN ./bootstrap.sh  --prefix=/usr/  \
-    --with-libraries=math,program_options,serialization,test
-RUN ./bjam install -j8
 
 EOF
 
-cat >> Dockerfile <<'EOF'
-
+cat >> Dockerfile << 'EOF'
 # Creating a non-root user.
 RUN adduser --system --disabled-password --disabled-login \
    --shell /bin/sh mlpack
@@ -64,28 +77,7 @@ RUN adduser --system --disabled-password --disabled-login \
 # Hardening the containers by unsetting all SUID tags
 RUN for i in `find / -perm 6000 -type f`; do chmod a-s $i; done
 
-
 # Changing work directory again.
 WORKDIR /home/mlpack
-
 USER mlpack
-
-# The following commands can be run to build and test different versions of
-# The following commands can be run to build and test different versions of
-# mlpack.
-
-# Downloading the source files and extracting them.
-#RUN wget -c http://www.mlpack.org/files/mlpack-2.2.0.tar.gz
-#RUN tar -xvzf mlpack-2.2.0.tar.gz
-
-
-# Making build directory.
-#RUN cd mlpack-2.2.0 && mkdir build
-#WORKDIR mlpack-2.2.0/build
-
-# Making the project.
-#RUN cmake  ../
-#RUN make
-#RUN bin/mlpack_test
-
 EOF
