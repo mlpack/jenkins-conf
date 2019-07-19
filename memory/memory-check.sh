6708f0161b989ddcbd6b547c330df43576fb6584
Ryan Curtin <ryan@ratml.org>
2019-07-18 21:17:36 -0400

    Try to get more output for why it's taking so long.

--- memory/memory-check.sh
+++ memory/memory-check.sh
@@ -10,4 +10,4 @@ xmllogfile=$(sed 's#/#_#g' <<<  $1).xml
 # Run valgrind.
 valgrind --tool=memcheck --leak-check=full --track-origins=yes \
          --num-callers=40 --error-exitcode=1 --xml=yes --xml-file=$REPORT_DIR/$xmllogfile --log-file=$REPORT_DIR/$logfile \
-         --verbose build/bin/mlpack_test -t $1
+         --verbose build/bin/mlpack_test -l all -t $1
