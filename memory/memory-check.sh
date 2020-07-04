476120c347358a1d46b3e3fddc41f558e78486b7
Ryan Curtin <ryan@ratml.org>
2020-07-04 16:33:08 -0400

    Run tests in the build directory.

--- memory/memory-check.sh
+++ memory/memory-check.sh
@@ -8,6 +8,8 @@ logfile=$(sed 's#/#_#g' <<<  $1).memcheck
 xmllogfile=$(sed 's#/#_#g' <<<  $1).xml
 
 # Run valgrind.
+cd build/
 valgrind --tool=memcheck --leak-check=full --track-origins=yes \
          --num-callers=40 --error-exitcode=1 --xml=yes --xml-file=$REPORT_DIR/$xmllogfile --log-file=$REPORT_DIR/$logfile \
-         --verbose build/bin/mlpack_test -l all -t $1
+         --verbose bin/mlpack_test -l all -t $1
+cd ../
