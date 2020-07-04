9751ea82e4b792dcdf6c01397a38e6ee19b489cd
Ryan Curtin <ryan@ratml.org>
2020-07-04 19:03:18 -0400

    Output boost test results to a separate file, for later parsing by Jenkins.

--- memory/memory-check.sh
+++ memory/memory-check.sh
@@ -5,11 +5,13 @@ REPORT_DIR="reports/tests"
 
 # Replace '/' with '_' from test bin to create logfile.
 logfile=$(sed 's#/#_#g' <<<  $1).memcheck
-xmllogfile=$(sed 's#/#_#g' <<<  $1).xml
+xmllogfile=$(sed 's#/#_#g' <<<  $1).valgrind.xml
+testxmlfile=$(sed 's#/#_#g' <<<  $1).boost_test.xml
 
 # Run valgrind.
 cd build/
 valgrind --tool=memcheck --leak-check=full --track-origins=yes \
          --num-callers=40 --error-exitcode=1 --xml=yes --xml-file=$REPORT_DIR/$xmllogfile --log-file=$REPORT_DIR/$logfile \
-         --verbose bin/mlpack_test -l all -t $1
+         --verbose bin/mlpack_test --report_level=detailed --log_level=test_suite --log_format=XML -t $1 \
+        > $testxmlfile;
 cd ../
