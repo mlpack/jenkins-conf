6c7955944ea406eaa7c3c23803b148989c5ac602
Marcus Edel <marcus.edel@fu-berlin.de>
2020-08-06 21:20:45 +0200

    Add script to parse ensmallen tests.

--- memory/run-ensmallen-valgrind-tests.sh
+++ memory/run-ensmallen-valgrind-tests.sh
@@ -4,13 +4,13 @@
 WORKER=1
 REPORT_DIR="reports/tests"
 CMAKE_FILE="./src/ensmallen/tests/CMakeLists.txt"
-TEST_PATH="./src/ensmallen/tests/"
 
 echo "Wipe out old reports."
 mkdir -p $REPORT_DIR
 rm -rf $REPORT_DIR/*
 
-echo "Running all tests."
+echo "Finding test cases."
+./parse-ensmallen-tests.py $CHANGED_FILES $CMAKE_FILE > testbins.txt
 
 # Replace '/' with '_' from test bin to create logfile.
 logfile=ensmallen.memcheck
@@ -19,7 +19,5 @@ xmllogfile=ensmallen.xml
 # Copy data to the correct directory.
 cp -vr build/data/ data/
 
-# Run valgrind.
-valgrind --tool=memcheck --leak-check=full --track-origins=yes \
-         --num-callers=40 --error-exitcode=1 --xml=yes --xml-file=$REPORT_DIR/$xmllogfile --log-file=$REPORT_DIR/$logfile \
-         --verbose build/ensmallen_tests
+echo "Running all tests."
+cat testbins.txt | xargs -n 1 -P $WORKER -I % ./memory-check.sh %
