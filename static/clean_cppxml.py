9bc6538f447260d8722379f237a1f7421404ec14
Ryan Curtin <ryan@ratml.org>
2020-08-12 13:30:50 -0400

    Add another file to the whitelist.

--- static/clean_cppxml.py
+++ static/clean_cppxml.py
@@ -19,7 +19,8 @@ whitelist = [("logistic_regression_main.cpp", "pointer was used after the memory
              ("best_binary_numeric_split_impl.hpp", "syntax error"),
              ("catch.hpp", "syntax error"),
              ("*", "Such expressions using the &apos;,&apos; operator are dangerous. Make sure the expression is correct."),
-             ("ra_search_impl.hpp", "syntax error")]
+             ("ra_search_impl.hpp", "syntax error"),
+             ("neighbor_search_impl.hpp", "syntax error")]
 
 def check_whitelist_issue(line):
     for w in whitelist:
