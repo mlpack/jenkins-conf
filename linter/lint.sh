daae2a3e3162762b1e8e6e1a8ea54e03ff89010f
Marcus Edel <marcus.edel@fu-berlin.de>
2020-11-01 14:30:05 +0100

    Ignore cereal header file.

--- linter/lint.sh
+++ linter/lint.sh
@@ -50,6 +50,7 @@ find "$dir" \
 ! -path "*src/mlpack/tests/catch.hpp" \
 ! -path "*src/mlpack/bindings/cli/third_party/CLI/CLI11.hpp" \
 ! -path "*src/mlpack/bindings/R/mlpack/src/boost/*" \
+! -path "*src/mlpack/core/cereal/pair_associative_container.hpp" \
 -print0 -iname '*.[hc]pp' -type f | \
 xargs -0 python "$linter"/cpplint.py --extensions=hpp,cpp --filter=\
 -whitespace/braces,\
