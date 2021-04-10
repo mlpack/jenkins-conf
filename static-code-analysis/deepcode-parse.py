78a169c1df6d9f4168cd8962798647e67c4ff3f4
Marcus Edel <marcus.edel@fu-berlin.de>
2021-04-10 04:39:07 +0200

    Relay on json output instead of txt.

--- static-code-analysis/deepcode-parse.py
+++ static-code-analysis/deepcode-parse.py
@@ -1,41 +1,18 @@
 import json
 import xml.sax.saxutils
-import re
 import os
 
-with open('deepcode.txt', mode="r", encoding="utf-8") as f:
-    issues = f.readlines()
-
 cwd = str(os.getcwd())
-ansiEscape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-with open('reports/deepcode-report.xml', 'w') as f:
-    i = 0
-    while i < len(issues):
-        if 'Warning issues' in issues[i]:
-            filename, msg = issues[i + 1].split(' ', 1)
-            linenumber = issues[i + 3].strip().split(' ')[1]
-            linenumber = linenumber.split(',')[0]
-            msg = ansiEscape.sub('', msg).strip()
-            filename = ansiEscape.sub('', filename).strip()
-            msg = xml.sax.saxutils.escape(msg, {'"' : "&quot;", "'" : "&apos;"}).strip()
+with open('deepcode.txt') as jf:
+    data = json.load(jf)
 
-            if len(filename) != 0:
-                f.write('''<error id="err" severity="error" msg="%s" verbose="%s">\n''' % (msg, msg))
-                f.write('''<location file="%s" line="%s"/>\n''' % (cwd + filename.strip(), linenumber))
-                f.write('''</error>\n''')
-            i += 3
-        elif 'missue helpers' in issues[i]:
-            linenumber = issues[i + 1].strip().split(' ')[1]
-            linenumber = linenumber.split(',')[0]
-            if len(issues[i + 2].split(' ', 1)) > 1:
-                filename, msg = issues[i + 2].split(' ', 1)
-                filename = ansiEscape.sub('', filename).strip()
-                msg = ansiEscape.sub('', msg).strip()
+    with open('reports/deepcode-report.xml', 'w') as f:
+        for filename in data["results"]["files"].keys():
+            for ffk, ffv in data["results"]["files"][filename].items():
+                msg = data["results"]["suggestions"][ffk]["message"]
                 msg = xml.sax.saxutils.escape(msg, {'"' : "&quot;", "'" : "&apos;"}).strip()
-                if len(filename) != 0:
-                    f.write('''<error id="err" severity="error" msg="%s" verbose="%s">\n''' % (msg, msg))
-                    f.write('''<location file="%s" line="%s"/>\n''' % (cwd + filename.strip(), linenumber))
-                    f.write('''</error>\n''')
-            i += 3
-        else:
-            i += 1
+                line = data["results"]["files"][filename][ffk][0]["rows"][0]
+
+                f.write('''<error id="err" severity="error" msg="%s" verbose="%s">\n''' % (msg, msg))
+                f.write('''<location file="%s" line="%s"/>\n''' % (cwd + filename.strip(), line))
+                f.write('''</error>\n''')
